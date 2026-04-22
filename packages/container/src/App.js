1fb0e54b306d5ab7ea2ee1b964353c59940d863f
SARHAN <k.sarhan311@gmail.com>
2026-04-22 17:21:38 +0200

    refactor: comment out unused useEffect hook in container App component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  useEffect(() => {
-    console.log("Container app is running");
-  }, []);
+  // useEffect(() => {
+  //   console.log("Container app is running");
+  // }, []);
   return (
     <div>
       <h1>Hi There</h1>

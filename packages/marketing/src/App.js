cd0ba9eb821fedd4b1d0268145effb44e4c79e60
SARHAN <k.sarhan311@gmail.com>
2026-04-25 16:18:59 +0300

    feat: integrate cross-app navigation synchronization using shared history objects and memory routing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -8,19 +8,19 @@ import {
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
-export default () => {
+export default ({ history }) => {
   const generateClassName = createGenerateClassName({
     productionPrefix: "mk",
     // disableGlobal: true,
   });
   return (
     <StylesProvider generateClassName={generateClassName} injectFirst>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
           <Route path="/" component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} onNavigate={onNavigate} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName, search }) {
+      const { pathname } = history.location;
+      if (nextPathName !== pathname) {
+        history.push(nextPathName, search);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
